fa63db41484dd5cb6b7bef9c17d0aa2be2d3457d
Ivan <ivan333ka@yandex.ru>
2023-07-30 18:21:43 +0700

    audit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import postcssNested from 'postcss-nested';
 import postcssImport from 'postcss-import';
 import postcssMixins from 'postcss-mixins';
 import postcssSimpleVars from 'postcss-simple-vars';
-import postcssComment from 'postcss-comment';
 import checker from 'vite-plugin-checker';
 import handlebarsPrecompile from './plugins/handlebars-precompile';
 
@@ -17,7 +16,6 @@ export default defineConfig(
     ],
     css: {
       postcss: {
-        parser: postcssComment,
         plugins: [
           postcssNested,
           postcssImport,
